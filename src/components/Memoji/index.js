7b7b945cd7653c527d50fb64e97ac457710536e8
Yuri Cunha <isyuricunha@duck.com>
2023-10-02 21:47:36 -0300

    yuri.png

--- src/components/Memoji/index.js
+++ src/components/Memoji/index.js
@@ -9,7 +9,7 @@ const Memoji = () => (
     <Image
       className={styles.image}
       alt="Yuri Cunha"
-      src="/images/me-two.jpeg"
+      src="/images/yuri.png"
       width={80}
       height={64}
     />
