aeb3781d53fd7003cf6c4fae1d0b2ec1c464a28b
Yuri <isyuricunha@duck.com>
2026-02-02 10:24:26 -0300

    Revert "feat(ui): show dynamic copyright year"
    This reverts commit 4b33686611fbb93d400186c2fedbec216e958d89.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,7 @@
 const Footer = () => {
-  const start_year = 2025
-  const current_year = new Date().getFullYear()
-  const year_label =
-    current_year > start_year
-      ? `${start_year}–${current_year}`
-      : `${start_year}`
-
   return (
     <footer className='mb-8 mt-16 flex justify-center'>
-      Yuri Cunha | &copy; {year_label}
+      Yuri Cunha | &copy; {new Date().getFullYear()}
     </footer>
   )
 }
